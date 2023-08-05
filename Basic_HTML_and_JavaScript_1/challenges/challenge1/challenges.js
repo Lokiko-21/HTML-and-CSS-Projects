69d752392d71fe26827f11702e6d5c064568913d
marco breton <marcoleal21@outlook.com>
2023-08-05 11:09:13 -0700

    rearranged files
    deleted files not needed, moved files and folders around to be more organized.

--- Basic_HTML_and_JavaScript_1/challenges/challenge1/challenges.js
+++
@@ -1,7 +0,0 @@
-function openForm() {
-    document.getElementById("my-form").style.display = "block";
-}
-
-function closeForm() {
-    document.getElementById("my-form").style.display = "none";
-}
