8cba24279e1899d0aa308ac1380108764fc715f4
marco breton <marcoleal21@outlook.com>
2024-01-13 17:22:58 -0800

    Started Project all over again!
    finished setup for the project including the frontend and backend as well as the database

---
+++ e-commerce/e_commerce/ecommerce/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def products(request):
+    return render(request, 'products.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def faq(request):
+    return render(request, 'faq.html')
