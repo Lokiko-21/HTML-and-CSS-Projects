9a31d7fa9087080662b6aa5bea5fbe3425a0169c
marco breton <marcoleal21@outlook.com>
2024-01-23 17:01:16 -0800

    made migrations
    made migrations for the following apps
    products
    faq
    home
    contact
    also started on building the products app.

--- ecommerce_project/myproject/myproject/settings.py
+++ ecommerce_project/myproject/myproject/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.products'
-    'apps.contact'
-    'apps.faq'
-    'apps.home'
+    'apps.products',
+    'apps.contact',
+    'apps.faq',
+    'apps.home',
 ]
 
 MIDDLEWARE = [
