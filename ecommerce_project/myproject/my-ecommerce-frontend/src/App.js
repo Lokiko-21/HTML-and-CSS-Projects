db0e28079cc5d129d1296edf5857f7175b6248eb
marco breton <marcoleal21@outlook.com>
2024-01-29 17:45:30 -0800

    set up routing in App.js
    configured routing in App.js for Home, Products, FAQ, and Contact.

--- ecommerce_project/myproject/my-ecommerce-frontend/src/App.js
+++ ecommerce_project/myproject/my-ecommerce-frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home/Home';
+import Products from '/Products/Products';
+import FAQ from './FAQ/FAQ';
+import Contact from './Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Switch>
+        <route exact path="/" component={Home} />
+        <route path="/products" component={Products} />
+        <route path="/faq" component={FAQ} />
+        <route path="contact" component={Contact} />
+      </Switch>
     </div>
+  </Router>
   );
 }
 
