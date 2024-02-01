c477e3122328ab40ba417a9e44b27bffcba88d49
marco breton <marcoleal21@outlook.com>
2024-01-31 17:32:33 -0800

    deleted project again...

--- ecommerce_project/myproject/my-ecommerce-frontend/src/App.js
+++ ecommerce_project/myproject/my-ecommerce-frontend/src/App.js
@@ -8,15 +8,15 @@ import Contact from './Contact/Contact';
 function App() {
   return (
     <Router>
-    <div>
-      <Switch>
-        <route exact path="/" component={Home} />
-        <route path="/products" component={Products} />
-        <route path="/faq" component={FAQ} />
-        <route path="contact" component={Contact} />
-      </Switch>
-    </div>
-  </Router>
+      <div>
+        <Switch>
+          <route exact path="/" component={Home} />
+          <route path="/products" component={Products} />
+          <route path="/faq" component={FAQ} />
+          <route path="contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
