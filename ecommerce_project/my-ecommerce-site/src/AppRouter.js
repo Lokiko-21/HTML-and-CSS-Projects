57663c9a36285f17ff2a5d48388f5ad70ad64a6e
marco breton <marcoleal21@outlook.com>
2024-02-25 12:21:05 -0800

    deleted everything

--- ecommerce_project/my-ecommerce-site/src/AppRouter.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './Home';
-import Products from './Products';
-import FAQ from './FAQ';
-import Contact from './Contact';
